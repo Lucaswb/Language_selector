3a56b3f53c106aeff6882a9783d79e2d68dffcdb
Lucas Blasdel <lwblasdel@gmail.com>
2019-06-07 11:37:12 -0700

    display the language at the bottom of the screen

--- js/scripts.js
+++ js/scripts.js
@@ -13,39 +13,52 @@ var prog = {
   "program12":"Basic",
   "program13":"Java",
 };
-alert(prog["program2"])
 
 
-//
-// $(document).ready(function() {
-//
-//   $("form#questionnaire").submit(function(event) {
-//     var prom1 = $("#name1").val().length;
-//     var prom2 = parseInt($("#age1").val());
-//     var prom3 = $("input:radio[name=question1]:checked").val();
-//     var prom4 = $("#question2").val();
-//     var prom5 = $("#question3").val();
-//     var prom6 = $("#question4").val();
-//
-//     if ((prom2>0)&&(prom1>0)){
-//       var result = (prom1*prom2*prom3*prom4*prom5*prom6)%13+1
-//
-//
-//
-//
-//     } else {
-//       alert("this program requires your age and your name");
-//     }
-//
-//
-//     // if ((prom1)||(prom2===N)) {
-//     //   alert("you must enter your age and name")
-//     // } else {
-//     //   alert(prom1*prom2)
-//     // }
-//
-//
-//
-//
-//   });
-// });
+
+$(document).ready(function() {
+
+  $("form#questionnaire").submit(function(event) {
+    var prom1 = $("#name1").val().length;
+    var prom2 = parseInt($("#age1").val());
+    var prom3 = parseInt($("#num1").val());
+    var prom4 = $("input:radio[name=question1]:checked").val();
+    var prom5 = $("#question2").val();
+    var prom6 = $("#question3").val();
+    var prom7 = $("#question4").val();
+
+
+    if ((prom2>0)&&(prom1>0)){
+      var result = (prom1*prom2*prom7*prom4*prom5*prom6*prom7)
+      if (prom3>0){
+        result+=prom3;
+        lang_num=(result%13)+1
+
+        alert("hi")
+
+
+      } else {
+        lang_num=(result%13)+1
+
+      }
+      var lang_name = prog["program"+lang_num]
+
+      $("#reveal").show()
+      $("#languageReveal p").append(lang_name);
+      event.preventDefault()
+    } else {
+      alert("this program requires your age and your name");
+    }
+
+
+    // if ((prom1)||(prom2===N)) {
+    //   alert("you must enter your age and name")
+    // } else {
+    //   alert(prom1*prom2)
+    // }
+
+
+
+
+  });
+});
