f33d1a45fc75201d38efcfb1ff6c07e20db999d2
Lucas Blasdel <lwblasdel@gmail.com>
2019-06-07 14:41:28 -0700

    fix issues with the button prompt in the header

--- js/scripts.js
+++ js/scripts.js
@@ -17,68 +17,61 @@ var prog = {
 
 
 $(document).ready(function() {
-  $("form#header").submit(function(event) {
+  $("button#action2").click(function(event){
+    alert("yes you do")
+    $("button#action2").hide()
+    event.preventDefault()
+  });
+  $("button#action1").click(function(event){
+    $("button#action1").hide()
+    $("button#action2").hide()
     $("form#questionnaire").show()
-    $("form#header .button").hide()
     event.preventDefault()
 
 
-  $("form#questionnaire").submit(function(event) {
-    var prom1 = $("#name1").val().length;
-    var prom2 = parseInt($("#age1").val());
-    var prom3 = parseInt($("#num1").val());
-    var prom4 = $("input:radio[name=question1]:checked").val();
-    var prom5 = $("#question2").val();
-    var prom6 = $("#question3").val();
-    var prom7 = $("#question4").val();
+    $("form#questionnaire").submit(function(event) {
+      var prom1 = $("#name1").val().length;
+      var prom2 = parseInt($("#age1").val());
+      var prom3 = parseInt($("#num1").val());
+      var prom4 = $("input:radio[name=question1]:checked").val();
+      var prom5 = $("#question2").val();
+      var prom6 = $("#question3").val();
+      var prom7 = $("#question4").val();
 
-
-
-
-    if ((prom1>0)&&(prom2>0)){
-      $("#errorAge").hide()
-      $("#errorName").hide()
-      var result = (prom1*prom2*prom7*prom4*prom5*prom6*prom7)
-      if (prom3>0){
-        result+=prom3;
-        lang_num=(result%13)+1
-      } else {
-        lang_num=(result%13)+1
-
-      }
-      var lang_name = prog["program"+lang_num]
-
-
-      $("#reveal").show()
-      $("#languageReveal p").remove();
-      $("#languageReveal").append("<p></p>");
-      $("#languageReveal p").append(lang_name);
-      event.preventDefault()
-
-    } else {
-      $("#reveal").hide()
-      if ((prom1===0)&&(prom2)){
+      if ((prom1>0)&&(prom2>0)){
         $("#errorAge").hide()
-        $("#errorName").show()
-      } else if ((!prom2) && (prom1)) {
         $("#errorName").hide()
-        $("#errorAge").show()
+        var result = (prom1*prom2*prom7*prom4*prom5*prom6*prom7)
+        if (prom3>0){
+          result+=prom3;
+          lang_num=(result%13)+1
+        } else {
+          lang_num=(result%13)+1
+
+        }
+        var lang_name = prog["program"+lang_num]
+
+
+        $("#reveal").show()
+        $("#languageReveal p").remove();
+        $("#languageReveal").append("<p></p>");
+        $("#languageReveal p").append(lang_name);
+        event.preventDefault()
+
       } else {
-        $("#errorAge").show()
-        $("#errorName").show()
+        $("#reveal").hide()
+        if ((prom1===0)&&(prom2)){
+          $("#errorAge").hide()
+          $("#errorName").show()
+        } else if ((!prom2) && (prom1)) {
+          $("#errorName").hide()
+          $("#errorAge").show()
+        } else {
+          $("#errorAge").show()
+          $("#errorName").show()
+        }
+        event.preventDefault()
       }
-      event.preventDefault()
-    }
-
-
-    // if ((prom1)||(prom2===N)) {
-    //   alert("you must enter your age and name")
-    // } else {
-    //   alert(prom1*prom2)
-    // }
-
-
-
     });
   });
 });
