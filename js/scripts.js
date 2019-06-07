93055f2148e96ee6881180e6665fff8ec5c3f9f6
Lucas Blasdel <lwblasdel@gmail.com>
2019-06-07 10:54:17 -0700

    return a programing language after enter is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,51 @@
+var prog = {
+  "program1":"C",
+  "program2":"C#",
+  "program3":"C++",
+  "program4":"Objective-C",
+  "program5":"Swift",
+  "program6":"Python",
+  "program7":"Java Script",
+  "program8":"R",
+  "program9":"PHP",
+  "program10":"Fortran",
+  "program11":"Ruby",
+  "program12":"Basic",
+  "program13":"Java",
+};
+alert(prog["program2"])
 
-$(document).ready(function() {
 
-  $("form#questionnaire").submit(function(event) {
-    var prom1 = $("#name1").val().length;
-    if (prom1===0) {
-      alert("you must enter a name")
-    } else {
-      alert("you entered a name")
-    }
-
-
-  });
-});
+//
+// $(document).ready(function() {
+//
+//   $("form#questionnaire").submit(function(event) {
+//     var prom1 = $("#name1").val().length;
+//     var prom2 = parseInt($("#age1").val());
+//     var prom3 = $("input:radio[name=question1]:checked").val();
+//     var prom4 = $("#question2").val();
+//     var prom5 = $("#question3").val();
+//     var prom6 = $("#question4").val();
+//
+//     if ((prom2>0)&&(prom1>0)){
+//       var result = (prom1*prom2*prom3*prom4*prom5*prom6)%13+1
+//
+//
+//
+//
+//     } else {
+//       alert("this program requires your age and your name");
+//     }
+//
+//
+//     // if ((prom1)||(prom2===N)) {
+//     //   alert("you must enter your age and name")
+//     // } else {
+//     //   alert(prom1*prom2)
+//     // }
+//
+//
+//
+//
+//   });
+// });
